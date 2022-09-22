d598a47acacd0cfec3f7a8122529f63700b7d4ae
Sudesh2024 <92080855+Sudesh2024@users.noreply.github.com>
2022-09-22 13:46:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+ }
+
